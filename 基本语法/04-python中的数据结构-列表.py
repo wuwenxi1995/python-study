f5dddfcf48b1358ccc61626de4b368446b70d169
wuwenxi <wenxi.wu@hand-china.com>
2025-04-02 18:08:29 +0800

    [fix] 数据结构

---
+++ 基本语法/04-python中的数据结构-列表.py
@@ -0,0 +1,43 @@
+"""
+List:列表 => [10,20,30] ['Tom','Jack','James']
+
+列表基本操作:
+    添加元素：append() 在列表末尾添加一个元素; extend() 将另一个列表添加到当前列表的末尾
+    count(x): 统计列表中元素 x 出现的次数
+    pop([i]): 删除列表中指定位置的元素，并返回被删除的元素; 如果未指定 i 则移除末尾的元素
+    copy(): 浅拷贝
+    index(x, start, end): 元素 x 在列表中的位置, 如果列表不包含元素x抛出异常; 可选参数
+            start, end 指定查询在元素x 在[start, end]范围内的子序列中的位置
+    reverse(): 反转列表
+    sort(*, key=None, reverse=False): 列表排序, key指定排序规则的函数; reverse一个
+            布尔值。若设置为 True，则列表会按照降序排列；若为 False（默认），则按升序排列
+            与 sorted() 函数的区别：
+            sort() 方法是列表对象的一个方法，它会直接修改原列表；而 sorted() 是一个内置
+            函数，它会返回一个新的排序后的列表，原列表保持不变
+"""
+
+fruits = ['orange', 'apple', 'pear', 'banana', 'kiwi', 'apple', 'banana']
+print("apple count: ", fruits.count("apple"))
+
+copy = fruits.copy()
+copy.extend(fruits)
+print("expend: ", copy)
+print("fruits contains: ", fruits.__contains__("tom"))
+
+'''
+ 排序
+'''
+# 按照值长度降序排序
+fruits.sort(key=len, reverse=True)
+print("sort: ", fruits)
+
+students = [
+    {"name": "Alice", "age": 20},
+    {"name": "Bob", "age": 18},
+    {"name": "Charlie", "age": 22}
+]
+
+sorted_student = sorted(students, key=lambda sort: sort["age"], reverse=True)
+print(f"sorted_student: {sorted_student}, students: {students}")
+students.sort(key=lambda sort: sort["age"], reverse=True)
+print("students: ", students)
