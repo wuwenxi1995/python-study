b21db426fc2c8af18d5955643aa76e85805388ba
wuwenxi <wenxi.wu@hand-china.com>
2025-05-27 17:28:42 +0800

    [fix] 异常&模块

---
+++ 基本语法/08-异常/01-异常捕获.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2025/5/27 11:15
+# @Author  : wuwenxi
+
+import logging
+from logging import getLogger, getLoggerClass
+
+logging.basicConfig(level=logging.ERROR)
+
+try:
+    print(a)
+except NameError as e:
+    print(e)
+
+
+def func():
+    raise Exception("不支持操作的方法")
+
+
+try:
+    func()
+except Exception as e:
+    print("使用raise抛出异常")
+    logging.error('使用raise抛出异常', exc_info=True)
